fdce2c68c41b88f2fe30e49523114edabab4e7cd
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-11-29 14:37:52 +0300

    Fix issues in index.js, update dependencies in package.jsonn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31; 
+const multiply = num1 * num2
+console.log(multiply);
+
+// RANDOM
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+// REMAINDER VALUE
+const num3 = 32;
+const num4 = 28;
+const mod = num3 % num4;
+console.log(mod);
+
+// MAXIMUM VALUE
+const max = Math.max(5,7.6,10,15,20);
+console.log(max);
